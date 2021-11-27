06abfd38901e4f948275ff4af175cdd59aabcdc0
Erickype <erickype@hotmail.com>
2021-11-27 15:42:26 -0500

    Pets-page and pets-form components added

---
+++ projects/pets-album/src/app/pets/components/pets-form/pets-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'alb-pets-form',
+  templateUrl: './pets-form.component.html',
+  styleUrls: ['./pets-form.component.css']
+})
+export class PetsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/pets-album/src/app/pets/pets.module.ts
+++ projects/pets-album/src/app/pets/pets.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PetsPageComponent } from './pages/pets-page/pets-page.component';
+import { PetsFormComponent } from './components/pets-form/pets-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PetsPageComponent,
+    PetsFormComponent
+  ],
   imports: [
     CommonModule
   ]
