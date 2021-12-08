8abd2e6e30f5d43cf3bf7b547a332e506e057cce
Erickype <erickype@hotmail.com>
2021-12-08 12:14:05 -0500

    perfect scrollbar added

--- projects/pets-album/src/app/shared/shared.module.ts
+++ projects/pets-album/src/app/shared/shared.module.ts
@@ -5,17 +5,33 @@ import { TableComponent } from './components/table/table.component';
 //material
 import {MatTableModule} from '@angular/material/table';
 
+//scrollbar
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX:true
+}
+
 @NgModule({
   declarations: [
     TableComponent
   ],
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    PerfectScrollbarModule
   ],
   exports:[
     TableComponent,
     MatTableModule
+  ],
+  providers:[
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
   ]
 })
 export class SharedModule { }
